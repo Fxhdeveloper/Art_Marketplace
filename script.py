dfeca8dbc0bcfb8aa43bf75ef605795e0470efac
Fxhdeveloper <festimxhoxhaj@gmail.com>
2019-02-06 14:45:08 +0100

    Add Listing and Client classes

--- script.py
+++ script.py
@@ -1,4 +1,37 @@
-
+class Listing:
+    def __init__(self, art, price, seller):
+      self.art    = art
+      self.price  = price
+      self.seller = seller
+  
+    def __repr__(self):
+      return("{art}, {price}".format(art = self.art.title, price = self.price))
+ 
+class Client:
+    def __init__(self, name, location, is_museum):
+        self.name      = name
+        self.is_museum = is_museum
+        if is_museum:
+            self.location  = location
+        else:
+            self.location  = "Private Collection"
+       
+    def sell_artwork(self, artwork, price):
+      if artwork.owner == self:
+        new_listing = Listing(artwork, price, self)
+        veneer.add_listing(new_listing)
+       
+    def buy_artwork(self, artwork):
+      if artwork.owner != self:
+        art_listing = None
+        for listing in veneer.listings:
+          if listing.art == artwork:
+            art_listing = listing
+            break
+        if art_listing != None:
+          art_listing.art.owner = self
+          veneer.remove_listing(art_listing)
+             
 class Marketplace:
     def __init__(self):
         self.listings = []
@@ -12,23 +45,29 @@ class Marketplace:
     def show_listings(self):
         for listing in self.listings:
             print(listing)
-       
-
-
-
 
 class Art:
-    def __init__(self, artist, title, medium, year):
+    def __init__(self, artist, title, medium, year, owner):
         self.artist = artist
         self.title  = title
         self.medium = medium
         self.year   = year
-
+        self.owner  = owner
+  
     def __repr__ (self):
-        return ('{artist}. "{title}". {year}, {medium} '.format(artist = self.artist, title  =  self.title, medium = self.medium, year   =  self.year))
+        return ('{artist}. "{title}". {year}, {medium}. {owner_name}, {owner_location}.'.format(artist = self.artist, title  =  self.title, medium = self.medium, year   =  self.year, owner_name = self.owner.name, owner_location = self.owner.location))
 
 
-new_art = Art()
-print(new_art)
+
 veneer = Marketplace()
-print(veneer.show_listings)
+edytta = Client('Edytta Halpirt', None , False )
+
+girl_with_mandolin = Art('Monet, Claude',"Girl with a Mandolin (Fanny Tellier)", "oil on canvas", 1910 , edytta)
+
+
+edytta.sell_artwork(girl_with_mandolin, "$6M (USD)")
+
+moma = Client('The MOMA', "New York", True )
+moma.buy_artwork(girl_with_mandolin)
+print(girl_with_mandolin)
+print(veneer.show_listings())
